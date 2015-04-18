ede483533477fbf05315856f26fbd8f1bc74b86c
Etienne Champagne <etienne.champagne.ec@gmail.com>
2015-04-18 12:54:42 +0200

    Basic Enemy implementation

--- src/MainScene.hx
+++ src/MainScene.hx
@@ -1,9 +1,13 @@
+import com.haxepunk.HXP;
 import com.haxepunk.Scene;
+import com.haxepunk.graphics.Image;
+
+import Enemy;
 
 class MainScene extends Scene
 {
 	public override function begin()
 	{
-
+		add(new Enemy(HXP.screen.width / 2, HXP.screen.height / 2, Image.createRect(50,100, 0xFF0000)));
 	}
 }
