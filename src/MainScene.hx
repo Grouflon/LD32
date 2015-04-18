f16aee0001b33c6a3d501b956a4d695537e71461
generalbrolys <generalbrolys@hotmail.fr>
2015-04-18 15:02:07 +0200

    parsing ok

--- src/MainScene.hx
+++ src/MainScene.hx
@@ -3,6 +3,7 @@ import com.haxepunk.Scene;
 import com.haxepunk.Entity;
 import com.haxepunk.graphics.Image;
 import Enemy;
+import Parsing;
 
 class MainScene extends Scene
 {
@@ -13,22 +14,7 @@ class MainScene extends Scene
 	
 	public override function begin()
 	{
-		var i:Int = 0;
-		var u:Int = 0;
-		
-		while (i < 632)
-		{
-			add(new SolidBlock(i, 400));
-			i += 32;
-		}
-		
-		while (u < 400)
-		{
-			add(new SolidBlock(0, u));
-			add(new SolidBlock(600, u));
-			u += 32;
-		}
-		
+		Parsing.createBlock(this);
 		add(new Enemy(HXP.screen.width / 2, HXP.screen.height / 2, Image.createRect(50,100, 0xFF0000)));
 	}
 }
