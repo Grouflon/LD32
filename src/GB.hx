9917bfb451147458393941632f5edaffb3412d7c
Etienne Champagne <etienne.champagne.ec@gmail.com>
2015-04-19 22:45:49 +0200

    Add enemy related Magic Numbers in GB

--- src/GB.hx
+++ src/GB.hx
@@ -13,21 +13,31 @@ class GB
 	{	
 	}
 	
-	// PLAYER RELATED
+	// PLAYER
 	static public var playerTallReach:Float 	= 9.5;
 	static public var playerShortReach:Float	= 7;
 	
 	static public var playerSpeed:Float			= 4.;
 	static public var playerLeglessSpeed:Float 	= 0.;
 	
-	// LIMBS RELATED
+	// LIMBS
 	static public var legBounceAttenuation:Float 	= 0.8;
 	static public var maxBounceAmount:Int 			= 3;
 	
-	// GAME RELATED
+	// GAME
 	static public var gravity:Vector2 	= new Vector2(0., 20.);
 		
-	// BOSS RELATED
+	// MELEE ENEMY 
+	static public var meleeSpeed = 60;
+	static public var meleeVisionRange = 150;
+	
+	// RANGE ENEMY
+	static public var rangeSpeed = 75;
+	static public var rangeVisionRange = 200;
+	static public var rangeEnemyProjectileSpeed = 200;
+	static public var rangeEnemyProjectileRange = 400;
+	
+	// BOSS
 	static public var limbToResist = 3;	
 	static public var initialBossArm = 0;
 	static public var initialBossLeg = 0;
