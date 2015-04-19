5bcf92cfbbf79e68873864c3f37fb41da9c16ad0
Etienne Champagne <etienne.champagne.ec@gmail.com>
2015-04-19 17:06:19 +0200

    Camera fix + buggy Boss

--- src/MainScene.hx
+++ src/MainScene.hx
@@ -23,14 +23,15 @@ class MainScene extends Scene
 		
 		levelHeight = level._height;
 		levelWidth = level._width;
+		
+		var boss : BossEnemy = new BossEnemy(null, 75, 100, 50, 50, 50, 200, 10);
+		add(boss);
 	}
 	
 	public override function update()
 	{	
 		if (player != null)
 		{	
-			trace("player x : " + player.x + " y : " + player.y);
-
 			if (player.x - HXP.halfWidth < 0)
 				HXP.camera.x = 0;
 			else if (player.x + HXP.halfWidth > levelWidth)
@@ -38,16 +39,18 @@ class MainScene extends Scene
 			else
 				HXP.camera.x = player.x - HXP.halfWidth;
 			
-			if (player.y + HXP.halfHeight > levelHeight)
+			/*if (player.y + HXP.halfHeight > levelHeight)
 				HXP.camera.y = levelHeight - HXP.height;
 			else if (player.y - HXP.halfHeight < 0)
 				HXP.camera.y = 0;
 			else	
-				HXP.camera.y = player.y - HXP.halfHeight;
+				HXP.camera.y = player.y - HXP.halfHeight;*/
+				HXP.camera.y = 0;
 		}
 		
 		super.update();
 	}
+	
 	
 	private var player:Player;
 	public var levelHeight:Int;
