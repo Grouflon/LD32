a17e977b041a5f1dd431e75796e0bc9b84893d86
Remi laptop <remi@uselessworks.com>
2015-04-20 20:55:17 +0200

    arm & leg pickups

--- src/ArmAmmunition.hx
+++ src/ArmAmmunition.hx
@@ -2,6 +2,8 @@ package;
 
 import com.haxepunk.Graphic;
 import com.haxepunk.graphics.Image;
+import com.haxepunk.graphics.Spritemap;
+import com.haxepunk.utils.Draw;
 
 import Ammunition;
 
@@ -14,9 +16,18 @@ class ArmAmmunition extends Ammunition
 
 	public function new(x:Float, y:Float) 
 	{
-		super(x, y, 7, 18);
+		var sprite:Spritemap = new Spritemap("graphics/arm_pickup.png", 33, 75);
+		sprite.add("float", [0, 0, 0, 1, 1, 2, 2, 3, 3, 3, 2, 2, 1, 1], 13);
+		super(x, y, 33, 75);
 		
-		addGraphic(Image.createRect(7, 18, 0x33CC33, 1));
+		originX = cast(Math.round(sprite.width * 0.5), Int);
+		originY = cast(Math.round(sprite.height * 0.5), Int);
+		
+		sprite.originX = originX;
+		sprite.originY = originY;
+		
+		addGraphic(sprite);
+		sprite.play("float");
 	}
 		
 		
@@ -25,4 +36,12 @@ class ArmAmmunition extends Ammunition
 		p.addArm();
 		super.addLimb(p);
 	}
+	/*
+	override public function render():Void
+	{
+		super.render();
+		
+		Draw.hitbox(this);
+		Draw.circle(cast(x, Int), cast(y, Int), 5, 0x00FF00);
+	}*/
 }
