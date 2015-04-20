edfdc6a9ecde53fd4cb1a3823759dbc197b93bd7
t.georgin <t.georgin@etu.isartdigital.fr>
2015-04-20 20:51:35 +0200

    Added first boss level prototype

--- src/LimbDummy.hx
+++ src/LimbDummy.hx
@@ -27,7 +27,7 @@ class LimbDummy extends Entity
 		else if (limbType == "arm") { var armImage:Image = new Image("graphics/arm.png"); addGraphic(armImage); }
 		
 		
-		_colorTween = new ColorTween(function (e:Dynamic) { HXP.world.remove(this); }, TweenType.OneShot);
+		_colorTween = new ColorTween(function (e:Dynamic) { HXP.scene.remove(this); }, TweenType.OneShot);
 		_colorTween.tween(0.5, 1, 1, 1., 0., Ease.quadOut);
 		addTween(_colorTween, true);
 		
@@ -47,6 +47,7 @@ class LimbDummy extends Entity
 	
 	override public function update():Void
 	{
+		super.update();
 		cast(graphic, Image).alpha = _colorTween.alpha;
 		moveBy(_velocity.x, _velocity.y);
 	}
