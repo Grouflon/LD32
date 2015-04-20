7f5f1f7a933b36d3bdd45d7ec3e8868df2810e52
Remi laptop <remi@uselessworks.com>
2015-04-20 21:18:34 +0200

    Exit sprite

--- src/LevelChanger.hx
+++ src/LevelChanger.hx
@@ -12,7 +12,7 @@ class LevelChanger extends Entity
 
 	public function new(_x :Int, _y : Int, _width : Int, _height : Int, _nextLevel : String) 
 	{
-		super(_x, _y, Image.createRect(_width, _height, 0xBB00FF));
+		super(_x, _y/*, Image.createRect(_width, _height, 0xBB00FF)*/);
 		collidable = true;
 		setHitbox(_width, _height, 0, 0);
 		layer = 150;
